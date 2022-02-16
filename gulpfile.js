aff0c7a1f675ea951390cb8bcbb6c3b9b149a085
bellsmarket-air <bellsmarketweb+air@gmail.com>
2022-02-17 04:11:31 +0900

    conf update

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,45 @@
 var gulp    = require('gulp'),
-    sass    = require('gulp-sass')(require('sass')),
-    plumber = require('gulp-plumber'),
-    connect = require('gulp-connect'),
-    notify  = require('gulp-notify'),
-    pug     = require('gulp-pug'),
-    rename  = require('gulp-rename'),
-    uglify  = require('gulp-uglify'),
-    sourcemaps    = require('gulp-sourcemaps'),
-    changed       = require('gulp-changed'),
-    cached       = require('gulp-cached'),
-    autoprefixer = require('gulp-autoprefixer');
-
-
+  sass    = require('gulp-sass')(require('sass')),
+  plumber = require('gulp-plumber'),
+  connect = require('gulp-connect'),
+  notify  = require('gulp-notify'),
+  pug     = require('gulp-pug'),
+  rename  = require('gulp-rename'),
+  uglify  = require('gulp-uglify'),
+  sourcemaps    = require('gulp-sourcemaps'),
+  changed       = require('gulp-changed'),
+  cached       = require('gulp-cached'),
+  autoprefixer = require('gulp-autoprefixer');
 var path = require('path');
 
-//path
-var paths = {
+
+// PATH
+const PATH = {
   dest: {
     html: './dest/',
     css: './dest/assets/css',
-    js: './dest/assets/js',
+    js: './dest/assets/js/*',
     img: './dest/assets/img',
     font: './dest/assets/font',
   },
   src: {
     pug: './src/pug',
-    scss: './src/scss',
+    scss: './src/scss/**/*.scss',
     js: './src/js',
     img: './src/img',
     font: './src/font',
   }
 };
 
+
 function showPathObj(obj) {
   for (let i in obj) {
     for (let j  in obj[i]) {
-     console.log(paths[i][j]);
+      console.log(PATH[i][j]);
     }
   }
   return false;
 }
-
-const main = () => {
-
-  console.log('call is main');
-  return true;
-};
-showPathObj(paths);
-
-// paths.doc_root = './/';
-// paths.css_src = 'src/sass/**/*.scss';
-// paths.css_dist = paths.doc_root + 'dest/assets/css/';
-// paths.js_src = 'src/**/*.js';
 
 
 function reload(done) {
@@ -62,23 +50,31 @@ function reload(done) {
   done();
 }
 
-function styles() {
+const styles = () => {
   return (
-    gulp.src('src/sass/styles.scss')
-    .pipe(plumber())
+    gulp.src(PATH.src.scss)
+    .pipe(sourcemaps.init())
+    .pipe(plumber({
+      errorHandler: notify.onError({
+        title: 'scss Compile Failed', // 任意のタイトルを表示させる
+        message: '<%= error.message %>' // エラー内容を表示させる
+      })
+    }))
     .pipe(sass())
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 3 versions'],
       cascade: false
     }))
     .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(gulp.dest('dest/assets/css'))
+    .pipe(gulp.dest(PATH.dest.css))
     .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sourcemaps.write('./'))
     .pipe(rename('styles.min.css'))
-    .pipe(gulp.dest('dest/assets/css'))
+    .pipe(gulp.dest(PATH.dest.css))
     .pipe(connect.reload())
+    .pipe(notify('Compile'))
   );
-}
+};
 
 
 // function compileAllSass(done) {
@@ -98,6 +94,7 @@ function styles() {
 // }
 
 
+// Wip
 function scripts() {
   return (
     gulp.src('src/js/scripts.js')
@@ -110,6 +107,8 @@ function scripts() {
   );
 }
 
+
+// Wip
 function html() {
   return (
     gulp.src('./dest/*.html')
@@ -119,13 +118,14 @@ function html() {
 }
 
 
-// pug 処理
+// Wip
+// Pug => html Compile
 function views() {
   return (
     gulp.src('src/pug/pages/**/*.pug')
     .pipe(plumber())
     .pipe(pug({
-        pretty: true
+      pretty: true
     }))
     .pipe(gulp.dest('./dest'))
     .pipe(connect.reload())
@@ -133,18 +133,19 @@ function views() {
 }
 
 
+// Wip
 // gulp.watch(ファイル, 処理)
 function watchTask(done) {
   gulp.watch('*.html', html);
-  gulp.watch('src/sass/**/*.scss', styles);
+  gulp.watch('src/scss/**/*.scss', styles);
   gulp.watch('src/js/scripts.js', scripts);
   gulp.watch('src/pug/pages/**/*.pug', views);
   done();
 }
 
 
-var watch = gulp.parallel(watchTask, reload);
-var build = gulp.series(gulp.parallel(styles, scripts, html, views));
+const watch = gulp.parallel(watchTask, reload);
+const build = gulp.series(gulp.parallel(styles, scripts, html, views));
 
 exports.reload = reload;
 exports.styles = styles;
@@ -154,3 +155,16 @@ exports.views = views;
 exports.watch = watch;
 exports.build = build;
 exports.default = watch;
+
+
+
+const main = () => {
+  console.log('call is main');
+
+  styles();
+
+  showPathObj(PATH);
+  return true;
+};
+
+main();
