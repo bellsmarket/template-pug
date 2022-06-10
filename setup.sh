8738925478290467b9dee73ba7d4ca71500c6912
bellsmarket-air <bellsmarketweb+air@gmail.com>
2022-06-11 05:39:31 +0900

    update

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BOOTSTRAP_TARGET=./node_modules/bootstrap
+if [ -d $BOOTSTRAP_TARGET ]; then
+  echo $BOOTSTRAP_TARGET
+  cp -i "./node_modules/bootstrap/scss/_variables.scss" "./src/scss/_base"
+  cp -i "./node_modules/bootstrap/scss/bootstrap.scss" "./src/scss"
+  cp -i "./node_modules/bootstrap/dist/js/bootstrap.bundle.js" "./src/js/"
+  cp -i "./node_modules/bootstrap/dist/js/bootstrap.bundle.js.map" "./src/js/"
+  cp -i "./node_modules/jquery/dist/jquery.js" "./src/js/"
+fi
