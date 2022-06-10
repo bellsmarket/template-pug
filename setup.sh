53bf1bacc268f42b990221d6e609c40fe87c6949
ccuu-p <cat.can.use.unix+mbp@gmail.com>
2022-06-11 05:43:58 +0900

    update

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 BOOTSTRAP_TARGET=./node_modules/bootstrap
 if [ -d $BOOTSTRAP_TARGET ]; then
   echo $BOOTSTRAP_TARGET
-  cp -i "./node_modules/bootstrap/scss/_variables.scss" "./src/scss/_base"
+  cp -i "./node_modules/bootstrap/scss/_variables.scss" "./src/scss/_base/_bs-variables.scss"
   cp -i "./node_modules/bootstrap/scss/bootstrap.scss" "./src/scss"
   cp -i "./node_modules/bootstrap/dist/js/bootstrap.bundle.js" "./src/js/"
   cp -i "./node_modules/bootstrap/dist/js/bootstrap.bundle.js.map" "./src/js/"
